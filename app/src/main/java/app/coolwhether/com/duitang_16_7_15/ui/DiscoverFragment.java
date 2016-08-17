e91b9b60c8e352e1e2511f4271dcd48985f4c47b
zzl <zzl@gmail.com>
2016-08-17 16:41:04 +0800

    DiscoverFragment功能完全实现了

--- app/src/main/java/app/coolwhether/com/duitang_16_7_15/ui/DiscoverFragment.java
+++ app/src/main/java/app/coolwhether/com/duitang_16_7_15/ui/DiscoverFragment.java
@@ -1,9 +1,24 @@
 package app.coolwhether.com.duitang_16_7_15.ui;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import app.coolwhether.com.duitang_16_7_15.R;
+import butterknife.ButterKnife;
 
 /**
  * Created by Administrator on 2016/8/15.
  */
 public class DiscoverFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.discoverfragment,container,false);
+        ButterKnife.bind(this,view);
+        return view;
+    }
 }
